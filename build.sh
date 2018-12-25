ae86034ba7001c467a013c3b5136985b4492f191
Yannick Schillo <yschillo@yansonline.de>
2018-12-25 21:58:44 +0100

    Remove build artifacts wasm/* from repository; Fancy waiting animation

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #/bin/sh
+mkdir -p wasm
 cargo build --release --target wasm32-unknown-unknown || exit 1
 wasm-bindgen target/wasm32-unknown-unknown/release/minifun_wasm.wasm --no-modules --no-typescript --out-dir wasm
